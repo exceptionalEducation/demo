b741024faf396f7699909c931754d052ef282d48
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-06-24 14:04:47 -0600

    starting express

--- twilio.js
+++ twilio.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 var twilio = require('twilio');
 var client = new twilio(process.env.accountSid, process.env.authToken);
-
+console.log('here');
+console.log(process.env.accountSid);
+console.log(process.env.authToken);
 client.messages.create({
     body: 'Hello from Node',
     to: '+18043473241',  // Text this number
